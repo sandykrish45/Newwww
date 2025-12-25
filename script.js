7bbf0276b5883cc490ae57b4042175a4a03d7e02
sandykrish45 <santy14.10.1999@gmail.com>
2025-12-25 17:36:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function showOptions(){
+ document.getElementById("options").style.display="block";
+}
+function reveal(n){
+ const msgs=[
+ "",
+ "You are my paasakaari 🫂",
+ "You are my lucky charm ❤️",
+ "You are my angel 👼",
+ "You are so special to me",
+ "My pondatti 💯"
+ ];
+ document.getElementById("message").innerText=msgs[n];
+}
